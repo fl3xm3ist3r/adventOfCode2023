05cef389757f01f68075a124d1c6d7574af8d351
Fl3xm3ist3r <artopien.net@gmail.com>
2023-12-30 14:56:28 +0100

    day 23

--- src/AdventOfCode2023/Days/Day23.cs
+++ src/AdventOfCode2023/Days/Day23.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace AdventOfCode2023.Days;
 
@@ -33,150 +32,57 @@ public class Day23 : DayBase
         return new ValueTask<string>(result.ToString());
     }
 
-    // doesn't work because it takes way to long
+    //6874 (takes approximately 6 minutes)
+    static char[][] visited = Array.Empty<char[]>();
+    public override ValueTask<string> Solve_2()
+    {
+        var map = GetInput(Input.Value).Select(l => l.Replace('<', '.').Replace('>', '.').Replace('^', '.').Replace('v', '.').ToCharArray()).ToArray();
 
-    // 6874
-    static int longestPath;
-    //public override ValueTask<string> Solve_2()
-    //{
-    //    var map = GetInput(Input.Value).Select(l => l.ToCharArray()).ToArray();
+        var startPoint = new string(map[0]).IndexOf('.');
+        var endPoint = new string(map[^1]).IndexOf('.');
 
-    //    var startIndex = new string(map[0]).IndexOf('.');
-    //    var endIndex = new string(map[^1]).IndexOf('.');
+        int maxDistance = 0;
 
-    //    var (startPoint, startNode) = (new Point(startIndex, 0), new Node());
-    //    var (endPoint, endNode) = (new Point(endIndex, map.Length - 1), new Node());
+        visited = new char[map.Length][];
+        for (int i = 0; i < map.Length; i++)
+        {
+            visited[i] = new char[map[i].Length];
+        }
 
-    //    var nodePointList = new List<(Point, Node)>
-    //    {
-    //        (startPoint, startNode),
-    //        (endPoint, endNode)
-    //    };
+        //instead of using a queue that blotes all my memory i used recursion this time and it worked
+        FindLongestPath(map, 0, startPoint, map.Length - 1, endPoint, ref maxDistance, 0);
 
-    //    for(int y = 0; y < map.Length; y++)
-    //    {
-    //        for(int x = 0; x < map[0].Length; x++)
-    //        {
-    //            if (map[y][x] == '#')
-    //            {
-    //                continue;
-    //            }
-
-    //            var tmpPoint = new Point(x, y);
-    //            if (IsNode(tmpPoint, map))
-    //            {
-    //                nodePointList.Add((tmpPoint, new Node()));
-    //            }
-    //        }
-    //    }
-
-    //    var nodeList = new List<Node>();
-    //    foreach(var (point, node) in nodePointList)
-    //    {
-    //        var directions = new Point[] {
-    //            new Point(-1, 0),
-    //            new Point(1, 0),
-    //            new Point(0, -1),
-    //            new Point(0, 1),
-    //        };
-
-    //        foreach (var direction in directions)
-    //        {
-    //            var tmpPoint = new Point(point.X, point.Y);
-
-    //            var stepCount = 0;
-    //            while (tmpPoint.Y + direction.Y >= 0 && tmpPoint.Y + direction.Y < map.Length &&
-    //                    tmpPoint.X + direction.X >= 0 && tmpPoint.X + direction.X < map[0].Length)
-    //            {
-    //                stepCount++;
-    //                tmpPoint.X += direction.X;
-    //                tmpPoint.Y += direction.Y;
-
-    //                if (map[tmpPoint.Y][tmpPoint.X] == '#')
-    //                {
-    //                    break;
-    //                }
-
-    //                if (IsNode(tmpPoint, map) || tmpPoint == endPoint)
-    //                {
-    //                    var (pointAtCoordinate, nodeAtCoordinate) = nodePointList.First(n => n.Item1 == tmpPoint);
-    //                    node.NearestNodes.Add((nodeAtCoordinate, stepCount));
-    //                    break;
-    //                }
-    //            }
-    //        }
-
-    //        nodeList.Add(node);
-    //    }
-
-    //    var queue = new Queue<NodePath>();
-    //    var startNodePath = new NodePath() {
-    //        Cache = new HashSet<Node>() { startNode },
-    //        Current = startNode
-    //    };
-    //    queue.Enqueue(startNodePath);
-
-    //    while (queue.Count > 0)
-    //    {
-    //        var path = queue.Dequeue();
-    //        MoveToNearestNodes(path, queue, endNode);
-    //    }
-
-    //    var result = longestPath;
-    //    return new ValueTask<string>(result.ToString());
-    //}
+        var result = maxDistance;
+        return new ValueTask<string>(result.ToString());
+    }
 
     private static List<string> GetInput(string input)
     {
         return input.Split($"{Environment.NewLine}").ToList();
     }
 
-    private static bool IsNode(Point point, char[][] map)
+    public static void FindLongestPath(char[][] map, int yStart, int xStart, int yEnd, int xEnd, ref int maxDistance, int distance)
     {
-        var possibleCorrectChars = new char[] { '.', '^', '>', 'v', '<' };
-
-        var up = point.Y - 1 >= 0 && possibleCorrectChars.Contains(map[point.Y - 1][point.X]);
-        var down = point.Y + 1 < map.Length && possibleCorrectChars.Contains(map[point.Y + 1][point.X]);
-        var left = point.X - 1 >= 0 && possibleCorrectChars.Contains(map[point.Y][point.X - 1]);
-        var right = point.X + 1 < map[0].Length && possibleCorrectChars.Contains(map[point.Y][point.X + 1]);
-
-        if((up && left) ||
-            (up && right))
+        if (yStart == yEnd && xStart == xEnd)
         {
-            return true;
+            maxDistance = Math.Max(maxDistance, distance);
+            return;
         }
 
-        if((down && left) ||
-            (down && right))
-        {
-            return true;
-        }
+        visited[yStart][xStart] = '.';
 
-        return false;
+        if (IsValid(yStart + 1, xStart, map)) FindLongestPath(map, yStart + 1, xStart, yEnd, xEnd, ref maxDistance, distance + 1);
+        if (IsValid(yStart - 1, xStart, map)) FindLongestPath(map, yStart - 1, xStart, yEnd, xEnd, ref maxDistance, distance + 1);
+        if (IsValid(yStart, xStart + 1, map)) FindLongestPath(map, yStart, xStart + 1, yEnd, xEnd, ref maxDistance, distance + 1);
+        if (IsValid(yStart, xStart - 1, map)) FindLongestPath(map, yStart, xStart - 1, yEnd, xEnd, ref maxDistance, distance + 1);
+
+        visited[yStart][xStart] = '\0';
     }
 
-    private static void MoveToNearestNodes(NodePath path, Queue<NodePath> queue, Node endNode)
+    public static bool IsValid(int y, int x, char[][] map)
     {
-        foreach (var (node, distance) in path.Current.NearestNodes.Where(n => !path.Cache.Contains(n.node)))
-        {
-            var totalDistance = path.Distance + distance;
-            if (node == endNode)
-            {
-                if (totalDistance > longestPath)
-                {
-                    longestPath = totalDistance;
-                }
-                continue;
-            }
-
-            var newPath = new NodePath()
-            {
-                Cache = new HashSet<Node>(path.Cache) { node },
-                Current = node,
-                Distance = totalDistance
-            };
-            queue.Enqueue(newPath);
-        }
+        if (x >= 0 && x < map[0].Length && y >= 0 && y < map.Length && map[y][x] == '.' && visited[y][x] != '.') return true;
+        return false;
     }
 
     public static void MoveInAllDirectionsIfPossible(Path path, Queue<Path> pathQueue, char[][] map, List<Path> successfullPaths)
@@ -241,17 +147,5 @@ public class Day23 : DayBase
         public Point Current { get; set; } = Point.Empty;
 
         public Point End {  get; set; } = Point.Empty;
-    }
-
-    public class Node()
-    {
-        public HashSet<(Node node, int distance)> NearestNodes { get; set; } = new HashSet<(Node, int distance)>();
-    }
-
-    public class NodePath()
-    {
-        public HashSet<Node> Cache { get; set; } = new HashSet<Node>();
-        public Node Current { get; set; } = new Node();
-        public int Distance { get; set; }
     }
 }
